e9fdcdd3fddf56d5ebfca129bd1f1eaef1b2abcf
uusa35 <uusa35@gmail.com>
2025-09-14 11:10:31 +0300

    updates

---
+++ public/build/assets/use-mobile-navigation-C8JA5zdh.js
@@ -0,0 +1,1 @@
+import{d as e}from"./app-Blfv5lCm.js";function t(){return e.useCallback(()=>{document.body.style.removeProperty("pointer-events")},[])}export{t as u};
--- public/build/assets/use-mobile-navigation-k-U1PeQb.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./app-C3YdaCXz.js";function t(){return e.useCallback(()=>{document.body.style.removeProperty("pointer-events")},[])}export{t as u};
